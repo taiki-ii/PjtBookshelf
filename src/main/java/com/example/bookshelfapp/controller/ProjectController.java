596f3dbe6038343268c74a1b3ae5e894b2fc3449
taiki-ii <yamiyami6110@gmail.com>
2026-04-15 00:52:44 +0900

    プロジェクト作成機能を追加

--- src/main/java/com/example/bookshelfapp/controller/ProjectController.java
+++ src/main/java/com/example/bookshelfapp/controller/ProjectController.java
@@ -4,10 +4,16 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.bookshelfapp.entity.Project;
+import com.example.bookshelfapp.form.ProjectCreateForm;
 import com.example.bookshelfapp.service.ProjectService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class ProjectController {
@@ -22,6 +28,22 @@ public class ProjectController {
 	public String showProjectList(Model model) {
 		List<Project> projectList = projectService.getProjectList();
 		model.addAttribute("projectList", projectList);
+		model.addAttribute("projectCreateForm", new ProjectCreateForm());
 		return "projects/list";
+	}
+	
+	@PostMapping("/creat-new-project")
+	public String registNewProject(
+			@Valid @ModelAttribute("projectCreateForm") ProjectCreateForm form,
+			BindingResult bindingResult,
+			Model model) {
+		
+		if(bindingResult.hasErrors()) {
+			return "projects/list";
+			
+		}
+		
+		projectService.createProject(form);
+		return "redirect:/projects";
 	}
 }
---
+++ src/main/java/com/example/bookshelfapp/form/ProjectCreateForm.java
@@ -0,0 +1,15 @@
+package com.example.bookshelfapp.form;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class ProjectCreateForm {
+	
+	@NotBlank(message = "プロジェクト名を入力してください。")
+	@Size(max = 255, message = "プロジェクト名は255文字以内で入力してください。")
+	private String projectName;
+	
+
+}
