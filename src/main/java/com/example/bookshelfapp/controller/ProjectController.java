85aa63303769eaf1039b1b5114445dc499a7a61b
taiki-ii <yamiyami6110@gmail.com>
2026-04-14 14:22:23 +0900

    プロジェクト一覧表示機能を作成しました

--- src/main/java/com/example/bookshelfapp/controller/ProjectController.java
+++ src/main/java/com/example/bookshelfapp/controller/ProjectController.java
@@ -1,5 +1,27 @@
 package com.example.bookshelfapp.controller;
 
-public class ProjectController {
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.bookshelfapp.entity.Project;
+import com.example.bookshelfapp.service.ProjectService;
+
+@Controller
+public class ProjectController {
+	
+	private final ProjectService projectService;
+	
+	public ProjectController(ProjectService projectService) {
+		this.projectService = projectService;
+	}
+	
+	@GetMapping("/projects")
+	public String showProjectList(Model model) {
+		List<Project> projectList = projectService.getProjectList();
+		model.addAttribute("projectList", projectList);
+		return "projects/list";
+	}
 }
--- src/main/java/com/example/bookshelfapp/service/ProjectService.java
+++ src/main/java/com/example/bookshelfapp/service/ProjectService.java
@@ -1,5 +1,22 @@
 package com.example.bookshelfapp.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.bookshelfapp.entity.Project;
+import com.example.bookshelfapp.repository.ProjectRepository;
+
+@Service
 public class ProjectService {
 
+    private final ProjectRepository projectRepository;
+
+    public ProjectService(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    public List<Project> getProjectList() {
+        return projectRepository.findAll();
+    }
 }
