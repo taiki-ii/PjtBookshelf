824f08aa84de5b82c7889e02af234f109cdd9e84
taiki-ii <yamiyami6110@gmail.com>
2026-04-18 23:30:00 +0900

    プロジェクト詳細画面の画面表示ができる　を追加

--- src/main/java/com/example/bookshelfapp/controller/ProjectWorkspaceController.java
+++ src/main/java/com/example/bookshelfapp/controller/ProjectWorkspaceController.java
@@ -1,5 +1,40 @@
 package com.example.bookshelfapp.controller;
 
-public class ProjectWorkspaceController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.example.bookshelfapp.service.ProjectWorkspaceService;
+
+@Controller
+public class ProjectWorkspaceController {
+	
+	private final ProjectWorkspaceService projectWorkspaceService;
+	
+	public ProjectWorkspaceController(ProjectWorkspaceService projectWorkspaceService) {
+		this.projectWorkspaceService = projectWorkspaceService;
+	}
+	
+	/**
+     * プロジェクト一覧画面
+     * 初期表示
+	 * 
+     */
+	@GetMapping("/projects/{projectId}/workspace")
+	public String showProjectWorkspace(
+			@PathVariable Integer projectId,
+			// @RequestParam(defalutValue = "shelf") String view,
+			// @RequestParam(defaultValue = "recent") String scope,
+			Model model) {
+		
+		model.addAttribute("projectId", projectId);
+		// model.addAttribute("view", view);
+		// model.addAttribute("scope, scope");
+		
+		// 共通情報
+		model.addAttribute("workspaceInfo", projectWorkspaceService.getWorkspaceInfo(projectId));
+		
+		return "projects/workspace";
+	}
 }
--- src/main/java/com/example/bookshelfapp/service/ProjectWorkspaceService.java
+++ src/main/java/com/example/bookshelfapp/service/ProjectWorkspaceService.java
@@ -1,5 +1,25 @@
 package com.example.bookshelfapp.service;
 
+import org.springframework.stereotype.Service;
+
+import com.example.bookshelfapp.entity.Project;
+import com.example.bookshelfapp.repository.ProjectRepository;
+import com.example.bookshelfapp.repository.UserRepository;
+
+@Service
 public class ProjectWorkspaceService {
+	
+	private final ProjectRepository projectRepository;
+	//private final UserRepository userRepository;
+	
+	public ProjectWorkspaceService(ProjectRepository projectRepository,UserRepository userRepository) {
+		this.projectRepository = projectRepository;
+		//this.userRepository = userRepository;
+	}
+	
+	public Project getWorkspaceInfo(Integer projectId) {
+		return projectRepository.findById(projectId)
+		.orElseThrow(() -> new IllegalArgumentException("指定したプロジェクトが存在しません。 id=" + projectId));
+	}
 
 }
